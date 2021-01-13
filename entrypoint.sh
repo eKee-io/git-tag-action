2f42c293e0d1f2f2f84b7184bececaf77bdcac68
Dot-H <alexandre.bernard@epita.fr>
2021-01-13 13:00:56 +0100

    use git command
    Signed-off-by: Dot-H <alexandre.bernard@epita.fr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,43 +6,21 @@ if [[ -z "${TAG}" ]]; then
    exit 1
 fi
 
+if [[ -z "${BRANCH}" ]]; then
+   echo "No branch supplied"
+   exit 1
+fi
+
 if [[ -z "${GITHUB_TOKEN}" ]]; then
    echo "No github token supplied"
    exit 1
 fi
 
-# check if tag already exists
-tag_exists="false"
-if [ $(git tag -l "$TAG") ]; then
-    tag_exists="true"
-fi
+# Delete the tag on any remote before pushing
+git push origin ":refs/tags/${TAG}"
 
-# push the tag to github
-git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+# Replace the tag to reference the most recent commit
+git tag -fa "${TAG}"
 
-echo "**pushing tag $tag to repo $GITHUB_REPOSITORY"
-
-if $tag_exists
-then
-  # update tag
-  curl -s -X PATCH "$git_refs_url/tags/$TAG" \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -d @- << EOF
-
-  {
-    "sha": "$GITHUB_SHA",
-    "force": true
-  }
-EOF
-else
-  # create new tag
-  curl -s -X POST $git_refs_url \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -d @- << EOF
-
-  {
-    "ref": "refs/tags/$TAG",
-    "sha": "$GITHUB_SHA"
-  }
-EOF
-fi
+# Push the tag to the remote origin
+git push -f origin "${BRANCH}" --tags
